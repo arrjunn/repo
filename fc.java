b57b12802de4b0339304633c0c52d0bc4931ff78
Arjun Varshney <arjunvarshney13@gmail.com>
2024-11-16 12:51:50 +0530

    chnges saved successfully

---
+++ fc.java
@@ -0,0 +1,12 @@
+public class fc{
+
+    // This is a public static void method
+    public static void printMessage() {
+        System.out.println("Hello from a static method!");
+    }
+
+    public static void main(String[] args) {
+        // Calling the static method directly without creating an object
+        printMessage();
+    }
+}
